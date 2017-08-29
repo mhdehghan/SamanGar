5756c8093a95f31731a221b37a392437c4447860
mhdehghan <mh.dehghan@ut.ac.ir>
2017-08-29 20:39:17 +0430

    Punctuation updated.

--- src/samanGar/normalizer/Normalizer.java
+++ src/samanGar/normalizer/Normalizer.java
@@ -5,8 +5,11 @@ import samanGar.sentence.Sentence;
 import samanGar.sentence.Word;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by dehghan on 8/24/2017.
@@ -14,52 +17,22 @@ import java.util.Map;
 public class Normalizer {
     Map<Character, Character> charSet;
     List<Character> validCharSet;
-    List<String> puncs;
 
     public Normalizer() {
-        puncs=new ReadData().readFile("resources/Normalizer/punctuations.txt");
-        charSet=new ReadData().readMap("resources/Normalizer/replaceChars.txt");
-        validCharSet=new ReadData().readChars("resources/Normalizer/validChars.txt");
+        charSet=new ReadData().readMap("resources/normalizer/replaceChars.txt");
+        validCharSet=new ReadData().readChars("resources/normalizer/validChars.txt");
     }
 
-    public Normalizer(String charSetFile,String validCharFile,String punctuationsFile) {
+    public Normalizer(String charSetFile,String validCharFile) {
         charSet = new ReadData().readMap(charSetFile);
         validCharSet=new ReadData().readChars(validCharFile);
-        puncs=new ReadData().readFile(punctuationsFile);
     }
 
-    public void process(Sentence sentence) {
-        replaceCharSet(sentence);
-        invalidChar(sentence);
-        splitPunctuation(sentence);
-        removeEmptyWords(sentence);
-    }
 
-    private void splitPunctuation(Sentence sentence){
-        String text=sentence.toString();
-        StringBuilder out = new StringBuilder();
-        for (String word: text.split(" ")) {
-            if ((word.length() > 1) && word.replaceAll("[0-9a-zA-Zا-ی]", "").length() > 0) {
-                for (int i = 0; i < word.length(); i++) {
-                    if (puncs.contains(word.substring(i, i + 1))) {
-                        if (i != 0) {
-                            out.append(" ");
-                        }
-                        out.append(word.substring(i, i + 1));
-                        if (i != word.length() - 1) {
-                            out.append(" ");
-                        }
-                    } else {
-                        out.append(word.substring(i, i + 1));
-                    }
-                }
-                out.append(" ");
-            } else {
-                out.append(word + " ");
-            }
-        }
-        sentence.setSentence(out.toString().trim());
-    }
+
+
+
+
 
     private void replaceCharSet(Sentence sentence) {
         for (int i = 0; i < sentence.length(); i++) {
@@ -98,5 +71,18 @@ public class Normalizer {
             if (sentence.get(i).trim().equals(""))
             {sentence.remove(i);i--;}
         }
+    }
+
+    public void process(Sentence sentence) {
+        replaceCharSet(sentence);
+        invalidChar(sentence);
+        //pullRegex(sentence);
+        //splitPunctuation(sentence);
+
+        //Add semi-space here!
+        //new SemiSpace().process(sentence);
+
+        removeEmptyWords(sentence);
+        //pushRegex(sentence);
     }
 }
